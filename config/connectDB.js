e1f7489bdfe0475abdd0dadc9668bff6497a388a
Nhat Truong <nhattruong@sapotacorp.vn>
2024-04-18 00:44:09 +0700

    18/4

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,28 +1,30 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('nhatit', 'root', null, {
+/* const sequelize = new Sequelize('nhatit', 'root', null, {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
 });
-
-/* const sequelize = new Sequelize('bsdjdwj3bdlkeblpo4of', 'ucc9rwxej6ig2nlu', 'chyeDmw2xTAuytxS766E', {
-  host: 'bsdjdwj3bdlkeblpo4of-mysql.services.clever-cloud.com',
-  dialect: 'mysql',
-  logging: false,
-});
  */
-
+const sequelize = new Sequelize(
+  "b1gtnii09lrzixp4r3g2",
+  "u1x2uwfspy0trykt",
+  "9k4uZAmwq0LcYKe3EfXY",
+  {
+    host: "b1gtnii09lrzixp4r3g2-mysql.services.clever-cloud.com",
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 let connectDB = async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-}
-
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
 
 module.exports = connectDB;
